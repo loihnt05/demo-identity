681d8aa0b21a7293939162d1af14465b5b900513
loihnt05 <23520869@gm.uit.edu.vn>
2025-04-28 19:25:51 +0700

    initial first

---
+++ src/main/java/com/superkids/demo_identity/DemoIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.superkids.demo_identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIdentityApplication.class, args);
+	}
+
+}
