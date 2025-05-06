63b68f706f7a1b693cad470a03744d9990a184b5
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-06 14:18:35 +0700

    advance validator annotation

--- src/main/java/com/superkids/demo_identity/exception/ErrorCode.java
+++ src/main/java/com/superkids/demo_identity/exception/ErrorCode.java
@@ -8,12 +8,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized Exception", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1111, "Can't find key exception", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1001, "User already existed", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1002, "Username must contain at least 3 characters", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1003, "Password must contain at least 8 characters", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002, "Username must contain at least {min} characters", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1003, "Password must contain at least {min} characters", HttpStatus.BAD_REQUEST),
     USERNAME_NOT_EXISTED(1004, "User not existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1005, "User not authenticated", HttpStatus.UNAUTHORIZED),
     FORBIDDEN(1006, "User not authorized", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1007, "Invalid date of birth", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1007, "Invalid date of birth, you must be at least {min}", HttpStatus.BAD_REQUEST),
     ;
 
     ErrorCode(int code, String message, HttpStatus status) {
--- src/main/java/com/superkids/demo_identity/exception/GlobalExceptionHandler.java
+++ src/main/java/com/superkids/demo_identity/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.superkids.demo_identity.exception;
 
 import com.superkids.demo_identity.dto.request.ApiResponse;
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.ErrorResponse;
@@ -9,10 +12,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.View;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     private final View error;
+    private final static String MIN_ATTRIBUTE = "min";
 
     public GlobalExceptionHandler(View error) {
         this.error = error;
@@ -52,12 +60,23 @@ public class GlobalExceptionHandler {
                         .build());
     }
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException e) {
         String enumKey = e.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
 
+        Map<String, Object> attributes = null;
+
         try{
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = e.getBindingResult()
+                    .getAllErrors()
+                    .getFirst()
+                    .unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
+
         }catch(IllegalArgumentException iae){
 
         }
@@ -65,8 +84,15 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getMessage(), attributes) :
+                errorCode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
